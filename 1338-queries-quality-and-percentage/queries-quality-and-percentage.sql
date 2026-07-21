86e80c93e43430e0c4b9dc06ec7ba5b7319ef85e
vaibhavavhad264 <167162006+vaibhavavhad264@users.noreply.github.com>
2026-07-21 16:39:57 +0530

    Time: 361 ms (73.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT query_name, 
+    IFNULL(ROUND(AVG(rating/position), 2), 0) AS quality, 
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
