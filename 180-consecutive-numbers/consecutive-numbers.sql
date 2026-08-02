4d3deaabefb8667662d885fc7c771a8ce594775c
vaibhavavhad264 <167162006+vaibhavavhad264@users.noreply.github.com>
2026-08-02 23:01:20 +0530

    Time: 661 ms (23.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+-- 02/08
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LAG(num) OVER (ORDER BY id) AS prev_num,
+        LEAD(num) OVER (ORDER BY id) AS next_num
+    FROM Logs
+) t
+WHERE num = prev_num
+  AND num = next_num;
+  
