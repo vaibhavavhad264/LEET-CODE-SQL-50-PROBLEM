7a91105516614d71b01bed712a504e2215df6048
vaibhavavhad264 <167162006+vaibhavavhad264@users.noreply.github.com>
2026-08-06 22:19:16 +0530

    Time: 696 ms (24.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+-- 06/08
+WITH PolicyCounts AS (
+    SELECT 
+        tiv_2016,
+        COUNT(*) OVER (PARTITION BY tiv_2015) AS count_tiv_2015,
+        COUNT(*) OVER (PARTITION BY lat, lon) AS count_location
+    FROM Insurance
+)
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM PolicyCounts
+WHERE count_tiv_2015 > 1 
+  AND count_location = 1;
